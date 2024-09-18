5ee5312712930d57ba789c154d6f43c3b33ded76
JordanChesley <jordan.r.chesley@gmail.com>
2024-09-18 13:28:53 -0400

    Turn skipping, end game, input loop

--- othello.py
+++ othello.py
@@ -8,6 +8,7 @@ WHITE_BOARD = [[]]
 WHITE = True
 BLACK = False
 
+# Track player scores.
 SCORE = {}
 
 # Expresions to apply to board dimensions during scanning tasks.
@@ -15,11 +16,12 @@ EXPRS = ["-1", "+0", "+1"]
 
 def print_board(playable_spaces: list=[], with_labels: bool=False):
   '''Prints the current board state. Set `with_labels` to `True` to print the row and column labels. '''
+
   # Board values:
   #    None: ' *'
   #    True: ' W'
   #   False: ' B'
-  print(f'Black - {SCORE[BLACK]}    White - {SCORE[WHITE]}')
+  print(f'\nBlack - {SCORE[BLACK]}    White - {SCORE[WHITE]}')
   if with_labels: print(' ', *range(1, BOARD_SIZE + 1))
   for i in range(BOARD_SIZE):
     if with_labels: print(i+1, end='')
@@ -33,6 +35,7 @@ def print_board(playable_spaces: list=[], with_labels: bool=False):
 
 def set_starting_config():
   '''Clear/Set the board and place the initial pieces.'''
+
   global WHITE_BOARD
   global SCORE
 
@@ -52,42 +55,48 @@ def set_starting_config():
 
 def get_piece(row: int, col: int):
   '''Returns piece value at a given row and column, or None if no piece exists.'''
+
   if ((row < 0 or row >= BOARD_SIZE) or (col < 0 or col >= BOARD_SIZE)): return None
   return WHITE_BOARD[row][col]
 
 
 def check_playable(color: bool, row: int, col: int, row_expr: str, col_expr: str, i: int=0):
   '''Recursive function that checks if a piece can be played at a certain space on the board.'''
+
+  # Get adjacent piece in the specified direction.
   adj_row = eval(f"{row}{row_expr}")
   adj_col = eval(f"{col}{col_expr}")
   piece = get_piece(adj_row, adj_col)
-  # print(f'Checking {row+1} {col+1}: {piece}') ### DEBUG
 
   # If the piece is non-existent, we cannot place the piece. Return False.
   if piece == None: return False
 
-  # In order to place our piece, the adjacent piece must be of the opposite color.
+  # During our first iteration, the immediate adjacent piece must be of the opposite color.
   if i == 0:
     if piece == color: return False
     elif piece != color:
       return check_playable(color, eval(f"{row}{row_expr}"), eval(f"{col}{col_expr}"), row_expr, col_expr, i+1)
+    
+  # For all other iterations:
 
-  # If the piece is the same color, we've determined that our pieces are flippable,
-  # and this one is already flipped. Return True.
+  # If the piece is the same color, we have a valid placement.
   elif piece == color: return True
 
-  # If piece is the other color, perform the expressions against our row and column integer. Continue to check if we can flip.
+  # If piece is the other color, continue checking.
   elif piece != color:
     return check_playable(color, adj_row, adj_col, row_expr, col_expr, i+1)
 
 
 def get_playable_spaces(color):
+  '''Returns all possible moves for the given color.'''
+
   playable_spaces = []
   # Iterate through our entire board.
   for row in range(BOARD_SIZE):
     for col in range(BOARD_SIZE):
       # Pieces can only be placed on empty spaces.
       if get_piece(row, col) == None:
+        # In each direction from our point, check if the piece can be placed.
         for row_expr in EXPRS:
           for col_expr in EXPRS:
             if (check_playable(color, row, col, row_expr, col_expr)):
@@ -98,6 +107,8 @@ def get_playable_spaces(color):
 
 def flip_pieces(color: bool, row: int, col: int, row_expr: str, col_expr: str):
   '''Recursive function that checks if pieces following a direction can be flipped, flips them if so, and updates the scores.'''
+
+  # Get adjacent piece.
   adj_row = eval(f"{row}{row_expr}")
   adj_col = eval(f"{col}{col_expr}")
   piece = get_piece(adj_row, adj_col)
@@ -127,35 +138,58 @@ def flip_pieces(color: bool, row: int, col: int, row_expr: str, col_expr: str):
 
 def play():
   '''Othello game loop.'''
+
   turn_order = [BLACK, WHITE]
   move = 0
+  skipped_turns = 0
 
   while True:
+    # If two consecutive turns have been skipped, the game is over; there are no more valid moves.
+    if skipped_turns == 2:
+      print('No more valid moves. Game over!')
+      print(f'Black - {SCORE[BLACK]}    White - {SCORE[WHITE]}')
+      return
+
     # Next color turn.
     team = turn_order[move % 2]
 
     # Scan for possible plays this color can make.
     playable_spaces = get_playable_spaces(team)
 
+    # If no spaces are available to play, skip their turn.
+    if len(playable_spaces) == 0:
+      print(f"{"WHITE" if team else "BLACK"}: Cannot place piece.")
+      skipped_turns += 1
+      move += 1
+      continue
+
     # Print the board.
     print_board(playable_spaces, with_labels=True)
 
-    # Black == Player. Ask player where they want to move.
-    if team == BLACK:
-      player_input = input("BLACK: Input row and column to place piece (e.g. \"0 0\"): ")
-      row, column = list(map(int, player_input.split()))
-      row -= 1; column -= 1
+    # Get input.
+    row = -1; column = -1
+    while (row, column) not in playable_spaces:
+      # Black == Player. Ask player where they want to move.
+      if team == BLACK:
+        player_input = input("BLACK: Input row and column to place piece (e.g. \"0 0\"): ")
+        splinput = player_input.split()
+        if (not splinput[0].isdigit()) or (not splinput[1].isdigit()):
+          print('Invalid input.')
+          continue
+        row = int(splinput[0]) - 1; column = int(splinput[1]) - 1
 
-    # White == AI. Ask AI for where it wants to move.
-    else:
-      ai_input = input("WHITE: Input row and column to place piece (e.g. \"0 0\"): ")
-      row, column = list(map(int, ai_input.split()))
-      row -= 1; column -= 1
-    
-    # Attempt to place a new piece.
-    if (row, column) not in playable_spaces:
-      print('Cannot place there.')
-      continue
+      # White == AI. Ask AI for where it wants to move.
+      else:
+        ai_input = input("WHITE: Input row and column to place piece (e.g. \"0 0\"): ")
+        splinput = ai_input.split()
+        if (not splinput[0].isdigit()) or (not splinput[1].isdigit()):
+          print('Invalid input.')
+          continue
+        row = int(splinput[0]) - 1; column = int(splinput[1]) - 1
+
+      # Do not allow player to place in an invalid space.
+      if (row, column) not in playable_spaces: print('Cannot place there.')
+
     # Place piece and increase score by one.
     WHITE_BOARD[row][column] = team
     SCORE[team] += 1
@@ -164,6 +198,9 @@ def play():
     for row_rule in EXPRS:
       for col_rule in EXPRS:
         flip_pieces(team, row, column, row_rule, col_rule)
+
+    # A successful turn has occurred. Reset skip counter.
+    skipped_turns = 0
 
     # Next move.
     move += 1
