a3e177fd0b0e101f50258af3d4c69d5d7e760f20
Jordan Chesley <jordan.r.chesley@gmail.com>
2024-09-18 11:34:19 -0400

    Added Playable Space Checking

--- othello.py
+++ othello.py
@@ -10,9 +10,10 @@ BLACK = False
 
 SCORE = {}
 
+# Expresions to apply to board dimensions during scanning tasks.
 EXPRS = ["-1", "+0", "+1"]
 
-def print_board(with_labels: bool=False):
+def print_board(playable_spaces: list=[], with_labels: bool=False):
   '''Prints the current board state. Set `with_labels` to `True` to print the row and column labels. '''
   # Board values:
   #    None: ' *'
@@ -23,10 +24,12 @@ def print_board(with_labels: bool=False):
   for i in range(BOARD_SIZE):
     if with_labels: print(i+1, end='')
     for j in range(BOARD_SIZE):
-      printchar = ' *' if WHITE_BOARD[i][j] == None else ' W' if WHITE_BOARD[i][j] else ' B'
+      if (i, j) in playable_spaces: printchar = ' o'
+      else: printchar = ' *' if WHITE_BOARD[i][j] == None else ' W' if WHITE_BOARD[i][j] else ' B'
       print(printchar, end='')
     print('')
   print('')
+
 
 def set_starting_config():
   '''Clear/Set the board and place the initial pieces.'''
@@ -36,7 +39,7 @@ def set_starting_config():
   # Clear the board / Set board dimensions.
   WHITE_BOARD = [[None for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
 
-  # Set scores to two.
+  # Set scores to two, as two pieces of each color will start on the board.
   SCORE = {BLACK: 2, WHITE: 2}
 
   # Place initial pieces onto the board.
@@ -46,21 +49,59 @@ def set_starting_config():
   WHITE_BOARD[center][center-1] = False
   WHITE_BOARD[center][center] = True
 
+
 def get_piece(row: int, col: int):
+  '''Returns piece value at a given row and column, or None if no piece exists.'''
   if ((row < 0 or row >= BOARD_SIZE) or (col < 0 or col >= BOARD_SIZE)): return None
   return WHITE_BOARD[row][col]
 
-def place_piece(color: bool, row: int, col: int):
-  # Check if the piece can be placed at the given spot.
-  if ((row < 0 or row >= BOARD_SIZE) or (col < 0 or col >= BOARD_SIZE)): return False
-  if (WHITE_BOARD[row][col] != None): return False
-  # If we can place, then place.
-  WHITE_BOARD[row][col] = color
-  return True
 
-def flip_pieces(color: bool, row: int, col: int, row_expr, col_expr):
-  piece = get_piece(row, col)
+def check_playable(color: bool, row: int, col: int, row_expr: str, col_expr: str, i: int=0):
+  '''Recursive function that checks if a piece can be played at a certain space on the board.'''
+  adj_row = eval(f"{row}{row_expr}")
+  adj_col = eval(f"{col}{col_expr}")
+  piece = get_piece(adj_row, adj_col)
   # print(f'Checking {row+1} {col+1}: {piece}') ### DEBUG
+
+  # If the piece is non-existent, we cannot place the piece. Return False.
+  if piece == None: return False
+
+  # In order to place our piece, the adjacent piece must be of the opposite color.
+  if i == 0:
+    if piece == color: return False
+    elif piece != color:
+      return check_playable(color, eval(f"{row}{row_expr}"), eval(f"{col}{col_expr}"), row_expr, col_expr, i+1)
+
+  # If the piece is the same color, we've determined that our pieces are flippable,
+  # and this one is already flipped. Return True.
+  elif piece == color: return True
+
+  # If piece is the other color, perform the expressions against our row and column integer. Continue to check if we can flip.
+  elif piece != color:
+    return check_playable(color, adj_row, adj_col, row_expr, col_expr, i+1)
+
+
+def get_playable_spaces(color):
+  playable_spaces = []
+  # Iterate through our entire board.
+  for row in range(BOARD_SIZE):
+    for col in range(BOARD_SIZE):
+      # Pieces can only be placed on empty spaces.
+      if get_piece(row, col) == None:
+        for row_expr in EXPRS:
+          for col_expr in EXPRS:
+            if (check_playable(color, row, col, row_expr, col_expr)):
+              playable_spaces.append((row, col))
+              # We only need to find at least one playable path. Break to reduce calculations.
+              break
+  return playable_spaces
+
+def flip_pieces(color: bool, row: int, col: int, row_expr: str, col_expr: str):
+  '''Recursive function that checks if pieces following a direction can be flipped, flips them if so, and updates the scores.'''
+  adj_row = eval(f"{row}{row_expr}")
+  adj_col = eval(f"{col}{col_expr}")
+  piece = get_piece(adj_row, adj_col)
+
   # If the piece is non-existent, we cannot flip. Return False.
   if piece == None: return False
 
@@ -70,12 +111,12 @@ def flip_pieces(color: bool, row: int, col: int, row_expr, col_expr):
 
   # If piece is the other color, perform the expressions against our row and column integer. Continue to check if we can flip.
   elif piece != color:
-    flippable = flip_pieces(color, eval(f"{row}{row_expr}"), eval(f"{col}{col_expr}"), row_expr, col_expr)
+    flippable = flip_pieces(color, adj_row, adj_col, row_expr, col_expr)
 
     # If flippable, flip the piece, and update the scores by one point.
     # If not flippable, do nothing.
     if (flippable):
-      WHITE_BOARD[row][col] = not piece
+      WHITE_BOARD[adj_row][adj_col] = not piece
       SCORE[color] += 1
       SCORE[not color] -= 1
       return True
@@ -85,6 +126,7 @@ def flip_pieces(color: bool, row: int, col: int, row_expr, col_expr):
   # We get here if the piece is the opposite color of the current playing color.
 
 def play():
+  '''Othello game loop.'''
   turn_order = [BLACK, WHITE]
   move = 0
 
@@ -92,8 +134,11 @@ def play():
     # Next color turn.
     team = turn_order[move % 2]
 
+    # Scan for possible plays this color can make.
+    playable_spaces = get_playable_spaces(team)
+
     # Print the board.
-    print_board(with_labels=True)
+    print_board(playable_spaces, with_labels=True)
 
     # Black == Player. Ask player where they want to move.
     if team == BLACK:
@@ -108,24 +153,22 @@ def play():
       row -= 1; column -= 1
     
     # Attempt to place a new piece.
-    placed = place_piece(team, row, column)
-    if not placed:
+    if (row, column) not in playable_spaces:
       print('Cannot place there.')
       continue
-    # Increase score by one.
+    # Place piece and increase score by one.
+    WHITE_BOARD[row][column] = team
     SCORE[team] += 1
 
     # Flip board pieces as necessary.
     for row_rule in EXPRS:
       for col_rule in EXPRS:
-        flip_pieces(team, eval(f'{row}{row_rule}'), eval(f"{column}{col_rule}"), row_rule, col_rule)
+        flip_pieces(team, row, column, row_rule, col_rule)
 
     # Next move.
     move += 1
 
 
 # MAIN PROGRAM
-
 set_starting_config()
-
 play()
