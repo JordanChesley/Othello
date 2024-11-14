e05058e69398cf045b2d0032316f5860fff02356
tommylt3 <77314289+tommylt3@users.noreply.github.com>
2024-11-14 00:44:12 -0500

    ugh

--- othello.py
+++ othello.py
@@ -1,14 +1,10 @@
-import numpy as np
-import pandas as pd
 
 
 class Game_Player:
 
     def __init__(self, name, color):
-        self.heatmap = [[5, 2, 4, 3, 3, 4, 2, 5], [2, 1, 2, 2, 2, 2, 1, 2], [4, 2, 3, 1, 1, 3, 2, 4], [3, 2, 1,	1,	1,	1,	2,	3], [
-            3, 2, 1,	1,	1,	1,	2,	3], [4, 2, 3, 1, 1, 3, 2, 4], [2, 1, 2, 2, 2, 2, 1, 2], [5, 2, 4, 3, 3, 4, 2, 5]]
         self.name = name
-        self.score = 0
+        self.score = 2
         self.color = color
 
     def set_color(self, color):
@@ -18,8 +14,8 @@ class Game_Player:
 class Bot (Game_Player):
 
     def __init__(self, name, color):
-        self.heatmap = np.array([[5, 2, 4, 3, 3, 4, 2, 5], [2, 1, 2, 2, 2, 2, 1, 2], [4, 2, 3, 1, 1, 3, 2, 4], [3, 2, 1, 1,	1,	1,	2,	3], [
-                                3, 2, 1,	1,	1,	1,	2,	3], [4, 2, 3, 1, 1, 3, 2, 4], [2, 1, 2, 2, 2, 2, 1, 2], [5, 2, 4, 3, 3, 4, 2, 5]])
+        self.heatmap = [[5, 2, 4, 3, 3, 4, 2, 5], [2, 1, 2, 2, 2, 2, 1, 2], [4, 2, 3, 1, 1, 3, 2, 4], [3, 2, 1, 1,	1,	1,	2,	3], [
+            3, 2, 1,	1,	1,	1,	2,	3], [4, 2, 3, 1, 1, 3, 2, 4], [2, 1, 2, 2, 2, 2, 1, 2], [5, 2, 4, 3, 3, 4, 2, 5]]
         self.name = f"Bot {name}"
         self.score = 0
         self.color = color
@@ -28,7 +24,7 @@ class Bot (Game_Player):
         if self.color == "Black":
             boardstate[boardstate == 1] = 2
             boardstate[boardstate == 0] = 1
-            boardstate[boardstate == 1] = 0
+            boardstate[boardstate == -1] = 0
         return boardstate
 
     def fitness_function(self, boardstate):
@@ -52,20 +48,12 @@ class Game:
         self.BOARD_SIZE = 8
 
         # Board is interpreted in terms of White pieces.
-        self.WHITE_BOARD = np.full((8, 8), np.nan)
+        self.WHITE_BOARD = [[None]*self.BOARD_SIZE] * self.BOARD_SIZE
 
         # Constant value configurations.
-        self.WHITE = 0
-        self.BLACK = 1
-
-        # Track player for turn order
-        self.players = [Player_A, Player_B]
-
-        # Scores Per Game Instance
-        self.score = {2, 2}
-
-        # Expressions to apply to board dimensions during scanning tasks.
-        self.EXPRS = ["-1", "+0", "+1"]
+        self.WHITE = Player_A
+        self.BLACK = Player_B
+        self.players = [self.WHITE, self.BLACK]
 
         self.set_starting_config()
         self.play()
@@ -76,13 +64,10 @@ class Game:
         temp_board = self.WHITE_BOARD
         temp_board[temp_board == 0] = "W"
         temp_board[temp_board == 1] = "B"
-        temp_board[temp_board == np.nan] = "*"
+        temp_board[temp_board == None] = "*"
         print(temp_board)
 
     def set_starting_config(self):
-
-        # Set scores to two, as two pieces of each color will start on the board.
-        self.SCORE = {self.BLACK: 2, self.WHITE: 2}
 
         # Place initial pieces onto the board.
         center = int(self.BOARD_SIZE / 2)
@@ -93,90 +78,21 @@ class Game:
 
     def get_piece(self, row: int, col: int):
         # Returns piece value at a given row and column, or None if no piece exists
-
         if ((row < 0 or row >= self.BOARD_SIZE) or (col < 0 or col >= self.BOARD_SIZE)):
             return None
         return self.WHITE_BOARD[row, col]
 
-    def check_playable(self, color: bool, row: int, col: int, row_expr: str, col_expr: str, i: int = 0):
-        # Recursive function that checks if a piece can be played at a certain space on the board.
+    # Recursive function that checks if a piece can be played at a certain space on the board.
+    def check_playable(self, color: bool, row: int, col: int, onRun: bool = False):
+        pass
 
-        # Get adjacent piece in the specified direction.
-        adj_row = eval(f"{row}{row_expr}")
-        adj_col = eval(f"{col}{col_expr}")
-        piece = self.get_piece(adj_row, adj_col)
-
-        # If the piece is non-existent, we cannot place the piece. Return False.
-        if piece == None:
-            return False
-
-        # During our first iteration, the immediate adjacent piece must be of the opposite color.
-        if i == 0:
-            if piece == color:
-                return False
-            elif piece != color:
-                return self.check_playable(color, eval(f"{row}{row_expr}"), eval(f"{col}{col_expr}"), row_expr, col_expr, i+1)
-
-        # For all other iterations:
-
-        # If the piece is the same color, we have a valid placement.
-        elif piece == color:
-            return True
-
-        # If piece is the other color, continue checking.
-        elif piece != color:
-            return self.check_playable(color, adj_row, adj_col, row_expr, col_expr, i+1)
-
+    # Recursive Function That Checks All Valid Spaces For A Player To Possibly Move
     def get_playable_spaces(self, color):
-
-        # Returns all possible moves for the given color
-        playable_spaces = []
-        # Iterate through our entire board.
-        for row in range(self.BOARD_SIZE):
-            for col in range(self.BOARD_SIZE):
-                # Pieces can only be placed on empty spaces.
-                if np.isnan(self.get_piece(row, col)):
-                    # In each direction from our point, check if the piece can be placed.
-                    for row_expr in self.EXPRS:
-                        for col_expr in self.EXPRS:
-                            if (self.check_playable(color, row, col, row_expr, col_expr)):
-                                playable_spaces.append((row, col))
-                                # We only need to find at least one playable path. Break to reduce calculations.
-                                break
-        return playable_spaces
+        for rows in self.BOARD_SIZE:
+            for cols in rows
 
     def flip_pieces(self, color: bool, row: int, col: int, row_expr: str, col_expr: str):
-        # Recursive function that checks if pieces following a direction can be flipped, flips them if so, and updates the scores.
-
-        # Get adjacent piece.
-        adj_row = eval(f"{row}{row_expr}")
-        adj_col = eval(f"{col}{col_expr}")
-        piece = self.get_piece(adj_row, adj_col)
-        x = np.where(np.diff(self.WHITE_BOARD) != 0) [1,0,0,nan,0,0,0,1]
-
-        # If the piece is non-existent, we cannot flip. Return False.
-        if piece == None:
-            return False
-
-        # If the piece is the same color, we've determined that our pieces are flippable,
-        # and this one is already flipped. Return True.
-        elif piece == color:
-            return True
-
-        # If piece is the other color, perform the expressions against our row and column integer. Continue to check if we can flip.
-        elif piece != color:
-            flippable = self.flip_pieces(
-                color, adj_row, adj_col, row_expr, col_expr)
-
-            # If flippable, flip the piece, and update the scores by one point.
-            # If not flippable, do nothing.
-            if (flippable):
-                self.WHITE_BOARD[adj_row][adj_col] = not piece
-                self.SCORE[color] += 1
-                self.SCORE[not color] -= 1
-                return True
-            else:
-                return False
+        pass
 
     # We get here if the piece is the opposite color of the current playing color.
     def play(self):
